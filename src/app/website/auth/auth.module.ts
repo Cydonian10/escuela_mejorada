40842ace0763cde2a5c49ef48e6a4038c9ee5dbc
Gabriel <depelos_07_@hotmail.com>
2022-01-24 07:08:27 -0500

    [login] la pagina de logearse

--- src/app/website/auth/auth.module.ts
+++ src/app/website/auth/auth.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { LoginMedicoComponent } from './pages/login-medico/login-medico.component';
+import { LayoutAuthComponent } from './components/layout-auth/layout-auth.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginMedicoComponent,
+    LayoutAuthComponent
   ],
   imports: [
     CommonModule,
